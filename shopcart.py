801b91d37fc0d84c02de7f842d413fea655b8baa
pnicholasti-dev <pnicholas.ti@gmail.com>
2026-04-16 18:37:48 -0300

    Add files via upload

---
+++ shopcart.py
@@ -0,0 +1,6 @@
+item = input("qual item você gostaria de comprar? :")
+preço = float(input("qual o valor dele?: "))
+quantidade = int(input("de quantos você gostaria? :"))
+total = preço * quantidade
+
+print(" Você deverá pagar: ", total)
